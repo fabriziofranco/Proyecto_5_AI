ccb3bd19eebe0a578bb44be42ad45a0f00f800e6
fabriziofranco <fabrizio.franco@utec.edu.pe>
2022-06-30 22:29:28 -0500

    Working

---
+++ gaaa/README.md
@@ -0,0 +1,1 @@
+# Proyecto_5_AI
---
+++ gaaa/utils.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import os
+from natsort import natsorted
+from PIL import Image
+from PIL import ImageFile
+from skimage import io, color, transform
+
+class LabDataSet(data.Dataset):
+    def __init__(self, main_dir, transform, test_size=0.1, height=128, width=128):
+        self.AB_scale = 128
+        self.main_dir = main_dir
+        self.transform = transform
+        self.gray_transform = transforms.Compose([transforms.ToTensor()]) 
+        self.height = height
+        self.width = width
+        
+        all_imgs = os.listdir(main_dir+'/color')
+        self.total_imgs = natsorted(all_imgs)
+
+        train_idx, val_idx = train_test_split(list(range(len(self.total_imgs))), test_size=test_size)
+        
+        self.train_set = [self.__getitem__(x) for x in train_idx ]
+        self.test_set = [self.__getitem__(x) for x in val_idx ]
+
+    def __len__(self):
+        return len(self.total_imgs)
+
+    def __getitem__(self, idx):
+        img_loc = os.path.join(self.main_dir+'/color', self.total_imgs[idx])
+        rgb = io.imread(img_loc)
+        rgb = transform.resize(rgb, (self.height, self.width), anti_aliasing=True)
+        lab = color.rgb2lab(rgb).astype("float32")
+        
+        tensor_image_gray = self.gray_transform(lab)
+        tensor_image_color = self.transform(lab)
+
+        tensor_image_gray = tensor_image_gray[0,:,:] /100
+        tensor_image_color = tensor_image_color[1:,:,:] / self.AB_scale
+        return (tensor_image_gray, tensor_image_color, img_loc)

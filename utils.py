a6e20414c06ceb1bda3ce86c9278f386f4cf47ba
fabriziofranco <fabrizio.franco@utec.edu.pe>
2022-07-01 23:54:02 -0500

    Working

--- utils.py
+++ utils.py
@@ -47,3 +47,59 @@ class LabDataSet(data.Dataset):
         tensor_image_color = tensor_image_color[1:,:,:] / self.AB_scale
 
         return (tensor_image_gray, tensor_image_color, img_loc)
+
+
+
+def plot_batch(device, test_loader, model = None ,height=256, width=256, step=32):
+    AB_scale = 128
+    model.eval()
+    with torch.no_grad():
+        iterator = iter(test_loader)
+        elements = next(iterator)
+
+        image_batch2 = torch.unsqueeze(elements[0], dim=1)
+        predictions = []
+        
+        resultado = model(image_batch2.to(device))
+
+        for pos in range(len(elements[0])):
+            L = (np.asarray(elements[0][pos])) * 100
+            A = (resultado[pos][0].detach().cpu().numpy()) *AB_scale
+            B = (resultado[pos][1].detach().cpu().numpy()) *AB_scale
+            datos = []
+            for i in range(L.shape[0]):
+                for j in range(L.shape[1]):
+                    datos.append([L[i,j], A[i,j], B[i,j]])
+
+            datos = np.reshape(datos, (height,width,3))
+            rgb = color.lab2rgb(datos)
+            predictions.append(rgb)
+
+
+        for pos in range(len(elements[0])):
+            if pos%step!=0:
+                continue
+            figure,axis = plt.subplots(1,3,figsize=(10,10))
+            axis[0].set_title("GRAY")
+            axis[1].set_title("COLOR")
+            axis[2].set_title("PREDICTED")
+
+            axis[0].axis("off")
+            axis[1].axis("off")
+            axis[2].axis("off")
+
+            L = (np.asarray(elements[0][pos])) * 100
+            A = np.asarray(elements[1][pos][0]) *AB_scale
+            B = np.asarray(elements[1][pos][1]) *AB_scale
+
+            datos = []
+            for i in range(L.shape[0]):
+                for j in range(L.shape[1]):
+                    datos.append([L[i,j], A[i,j], B[i,j]])
+
+            datos = np.reshape(datos, (height,width,3))
+            rgb = color.lab2rgb(datos)
+            axis[0].imshow(L, cmap='gray')
+            axis[1].imshow(rgb)
+            axis[2].imshow(predictions[pos])
+            plt.show()
